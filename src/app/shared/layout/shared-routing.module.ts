f06d3346f231fee0e6d528bfd37a9604f3d5ec53
Grishm Rathod <grathod49@gmail.com>
2022-11-24 03:28:26 +0530

    module change

--- src/app/shared/layout/shared-routing.module.ts
+++ src/app/shared/layout/shared-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
 ];
